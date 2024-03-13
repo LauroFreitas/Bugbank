4d5219dbc28fe041696cdf0ba08e3a3ee1d5197f
laurofreitas <lauroh.sfreitas@gmail.com>
2024-03-13 20:34:52 -0300

    Cria e salva duas contas

--- automacao_bugbank/src/test/java/automacao_bugbank/novo_cadastro.java
+++ automacao_bugbank/src/test/java/automacao_bugbank/novo_cadastro.java
@@ -30,8 +30,36 @@ public class novo_cadastro {
     }
 
     @Test
+    public void testMain() {
+    	driver.get("https://bugbank.netlify.app/");
+    	criaConta_UserUm();
+    	driver.get("https://bugbank.netlify.app/");
+    	criaConta_UserDois();
+        driver.findElement(By.cssSelector(".CMabB")).click();
+        {
+          WebElement element = driver.findElement(By.cssSelector(".CMabB"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element).perform();
+        }
+        {
+          WebElement element = driver.findElement(By.tagName("body"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element, 0, 0).perform();
+        }
+        driver.findElement(By.id("btnCloseModal")).click();
+        
+        driver.findElement(By.name("email")).sendKeys(email_usuario_um);
+        driver.findElement(By.name("password")).sendKeys(password_userUm);
+        
+        driver.findElement(By.cssSelector(".otUnI")).click();
+        {
+          WebElement element = driver.findElement(By.cssSelector(".otUnI"));
+          Actions builder = new Actions(driver);
+          builder.moveToElement(element).perform();
+        }
+    
+    }
     public void criaConta_UserUm() {
-        driver.get("https://bugbank.netlify.app/");
         assertEquals("BugBank | O banco com bugs e falhas do seu jeito", driver.getTitle());
         driver.findElement(By.xpath("//button[contains(text(), 'Registrar')]")).click();
         driver.findElement(By.cssSelector(".style__ContainerFieldInput-sc-s3e9ea-0:nth-child(2) > .input__default")).sendKeys(email_usuario_um);
@@ -49,12 +77,32 @@ public class novo_cadastro {
         actions.moveToElement(submit).click().perform();
         
         driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]")).click();
+       
+    }
+    public void criaConta_UserDois() {
+        assertEquals("BugBank | O banco com bugs e falhas do seu jeito", driver.getTitle());
+        driver.findElement(By.xpath("//button[contains(text(), 'Registrar')]")).click();
+        driver.findElement(By.cssSelector(".style__ContainerFieldInput-sc-s3e9ea-0:nth-child(2) > .input__default")).sendKeys(email_usuario_dois);
+        driver.findElement(By.name("name")).sendKeys("Mauro");
+        driver.findElement(By.cssSelector(".login__password:nth-child(4) .input__default")).sendKeys(password_userDois);
+        driver.findElement(By.name("passwordConfirmation")).sendKeys(password_userDois);
+        
+        WebElement element = driver.findElement(By.id("toggleAddBalance"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+        
+        driver.findElement(By.id("toggleAddBalance")).click();
+        
+        WebElement submit = driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]"));
+        actions.moveToElement(submit).click().perform();
+        
+        driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]")).click();
         
     }
 
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
 
 }
